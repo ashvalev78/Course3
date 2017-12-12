17133f04487d39699a45cc1a8ed0ae304a5d5a8d
Aleksandr <ashvalev78@gmail.com>
2017-12-12 17:30:29 +0300

    vector is donegit add .git add .

--- main.cpp
+++ main.cpp
@@ -1,170 +1,136 @@
 #include <iostream>
+#include <fstream>
 #include <list>
 #include <stack>
-#include <stdio.h>
-#include <stdlib.h>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
-#define MIN_PER_RANK 1 /* Nodes/Rank: How 'fat' the DAG should be.  */
-#define MAX_PER_RANK 5
-#define MIN_RANKS 3    /* Ranks: How 'tall' the DAG should be.  */
-#define MAX_RANKS 5
-#define PERCENT 70     /* Chance of having an Edge.  */
+const string PATH = "/Users/artemkaloev/Desktop/study/Course3/graph.dot";
 
+enum color {
+    WHITE,
+    GREY,
+    BLACK
+};
 
-// Class to represent a graph
+struct node {
+    int num;
+    vector<node*> siblings;
+};
+
 class Graph {
-    int V;    // No. of vertices&#039;
-
-    // Pointer to an array containing adjacency listsList
-    list<int> *adj;
-
-    // A function used by topologicalSort
-    bool topologicalSortUtil(int v, int visited[], stack<int> &Stack);
+    int V;
+    node *nodeArray;
+//    list<int> *adj;
+    bool topologicalSortUtil(int v, int color[], stack<int> &Stack);
 
 public:
-    Graph(int V);   // Constructor
-
-    // function to add an edge to graph
+    Graph(int V);
+//    Graph(const Graph &old);
     void addEdge(int v, int w);
-
-    // prints a Topological Sort of the complete graph
-    void topologicalSort();
-
-    void generator (int inputedNodes)
-    {
-        int i, j, k, nodes = inputedNodes;
-
-        int ranks = MIN_RANKS
-                    + (rand () % (MAX_RANKS - MIN_RANKS + 1));
-
-        printf ("digraph {\n");
-//        for (i = 0; i < ranks; i++)
-//        {
-            /* New nodes of 'higher' rank than all nodes generated till now.  */
-//            int new_nodes = rand()%nodes;
-//                    MIN_PER_RANK
-//                            + (rand () % (MAX_PER_RANK - MIN_PER_RANK + 1));
-
-            /* Edges from old nodes ('nodes') to new ones ('new_nodes').  */
-            for (j = 0; j < nodes; j++)
-                for (k = j + 1; k < nodes; k++)
-                    if ( (rand () % 100) < PERCENT) {
-                        cout << j << "->" << k << endl;
-                        addEdge(j, k); /* An Edge.  */
-                    }
-//            cout << "OK3" << endl;
-////            nodes += new_nodes; /* Accumulate into old node set.  */
-//            cout << "OK4" << endl;
-//        }
-        printf ("}\n");
-    }
-
+    bool topologicalSort();
+    void generator();
+    //деструктор
 };
 
 Graph::Graph(int V) {
     this->V = V;
-    adj = new list<int>[V];
+    nodeArray = new node[V];
+    for (int i = 0; i < V; i++) {
+        nodeArray[i].num = i;
+    }
+//    ofstream fout;
+//    fout.open(PATH);
+//    if (!fout.is_open()) cout << "Cannot open the file\n";
+//
+//    fout << "digraph {\n}";
+//    fout.close();
+    //очистить файл .dot
+    //добавить инициализацию графа в файл .dot
 }
 
 void Graph::addEdge(int v, int w) {
-    adj[v].push_back(w); // Add w to v’s list.
+    cout << v << "->" << w << endl;
+    nodeArray[v].siblings.push_back(&nodeArray[w]);
+//    for (auto i = nodeArray[v].siblings.begin(); i < nodeArray[v].siblings.end(); i++)
+//        cout << (*i)->num;
+//
+//    ofstream fout;
+//    fout.open(PATH, ios::in);
+//    if (!fout.is_open()) cout << "Cannot open the file\n";
+//
+//    fout.seekp(-1, ios::end);
+//    fout << "\t"<< v << " -> " << w << ";\n}";
+//    fout.close();
+    //добавить строку в файл .dot
 }
 
-// A recursive function used by topologicalSort
-bool Graph::topologicalSortUtil(int v, int visited[],
-                                stack<int> &Stack) {
-// Mark the current node as visited.
-    if (visited[v] == 1) {
-        cout << "cycle detected" << endl;
+bool Graph::topologicalSortUtil(int v, int color[], stack<int> &Stack) {
+    cout << "v = " << v << endl;
+    if (color[v] == GREY) {
+        cout << "Cycle detected => ";
         return false;
     }
-    visited[v] = 1;
+    cout << "OK" << endl;
+    color[v] = GREY;
+    cout << "OK2" << endl;
+    int j = v;
+    for (auto i = nodeArray[v].siblings.begin(); i != nodeArray[v].siblings.end(); ++i, ++j)
+        if (color[j] != BLACK) {
+            if (!topologicalSortUtil(nodeArray[v].siblings[j]->num, color, Stack)) return false;
+        }
 
-// Recur for all the vertices adjacent to this vertex
-    list<int>::iterator i;
-    for (i = adj[v].begin(); i != adj[v].end(); ++i)
-        if (visited[*i] != 2)
-            if (!topologicalSortUtil(*i, visited, Stack)) return false;
-
-// Push current vertex to stack which stores result
-    visited[v] = 2;
+    color[v] = BLACK;
     Stack.push(v);
     return true;
 }
 
-// The function to do Topological Sort. It uses recursive
-// topologicalSortUtil()
-void Graph::topologicalSort() {
+bool Graph::topologicalSort() {
     stack<int> Stack;
 
-    // Mark all the vertices as not visited
-    auto *visited = new int[V];
+    auto *color = new int[V];
     for (int i = 0; i < V; i++)
-        visited[i] = 0;
+        color[i] = WHITE;
 
-    // Call the recursive helper function to store Topological
-    // Sort starting from all vertices one by one
     for (int i = 0; i < V; i++) {
-        if (visited[i] == 0)
-            if (!topologicalSortUtil(i, visited, Stack)) {
-
-                // Print contents of stack
-                cout << "ti govno" << endl;
+        if (color[i] == WHITE)
+            if (!topologicalSortUtil(i, color, Stack)) {
+                cout << "Topological sort of the given graph is impossible";
+                while(!Stack.empty()) Stack.pop();
+                return false;
             }
     }
-    int i = 0;
+
+    cout << "\nFollowing is a Topological Sort of the given graph:\n";
+
     while (!Stack.empty()) {
         cout << Stack.top() << " ";
-//
-        i++;
-//        cout << Stack.top() << " ";
         Stack.pop();
     }
-    cout << endl;
+    return true;
 }
 
-//bool topological_sort() {
-//    bool Cycle;
-//    for (int i = 1; i <= N; i++) {
-//        Cycle = dfs(i);
-//        if (Cycle)return false;
-//    }
-//    for (int i = 1; i <= n; i++) {
-//        Numbers[Stack.pop()] = i;
-//    }
-//    return true;
-//}
-//
-//bool dfs(int v) {
-//    if (Color[v] == 1)return true;
-//    if (Color[v] == 2)return false;
-//    Color[v] = 1;
-//    for (int i = 0; i < Edges[v].size(); i++) {
-//        if (dfs(Edges[v].get(i)))return true;
-//    }
-//    Stack.push(v);
-//    Color[v] = 2;
-//    return false;
-//}
+void Graph :: generator ()
+{
+    const int PERCENT = 40;
+    for (int j = 0; j < V; j++)
+        for (int k = j + 1; k < V; k++)
+            if ( (rand () % 100) < PERCENT) {
+                addEdge(j, k);
+            }
+}
 
-// Driver program to test above functions
 int main() {
-    srand((unsigned int) time(nullptr));
+    srand(time(nullptr));
     // Create a graph given in the above diagram
-    Graph g(4);
-    g.generator(4);
-//    g.addEdge(0, 1);
-//    g.addEdge(0, 2);
-//    g.addEdge(3, 1);
-//    g.addEdge(3, 2);
-//    g.addEdge(0, 2);
-//    g.addEdge(2, 1);
-
-    cout << "Following is a Topological Sort of the given graph \n" << endl;
+    Graph g(5);
+    g.generator();
     g.topologicalSort();
+
+//    Graph n(g);
+//    n.topologicalSort();
 
     return 0;
 }
